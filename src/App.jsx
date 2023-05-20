6506d432fe331e05c71e1a81980971ad07bda9bb
Ana Toplicanec <ana.toplicanec@gmail.com>
2023-05-20 20:40:18 +0200

    input komponenta

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,47 @@
 import React, { Component } from 'react';
 import './App.css';
-import Messages from "./components/Messages";
-import { randomColor, randomName } from './components/RandomUser';
+import Messages from "./Messages";
+import { randomColor, randomName } from './RandomUser';
+import Input from "./Input"
 
 
 class App extends Component {
- 
+  state = {
+    messages: [
+      {
+        text: "This is a test message!",
+        member: {
+          color: "blue",
+          username: "bluemoon"
+        }
+      }
+    ],
+    member: {
+      username: randomName(),
+      color: randomColor()
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <Messages
+          messages={this.state.messages}
+          currentMember={this.state.member}
+        />
+        <Input
+          onSendMessage={this.onSendMessage}
+        />
+      </div>
+    );
+  }
+  onSendMessage = (message) => {
+    const messages = this.state.messages
+    messages.push({
+      text: message,
+      member: this.state.member
+    })
+    this.setState({messages: messages})
+  }
 }
 
 export default App;
---
+++ src/Input.jsx
@@ -0,0 +1,26 @@
+import {Component} from "react";
+import React from "react";
+
+class Input extends Component {
+  state = {
+    text: ""
+  }
+  render() {
+    return (
+      <div className="Input">
+        <form onSubmit={e => this.onSubmit(e)}>
+          <input
+            onChange={e => this.onChange(e)}
+            value={this.state.text}
+            type="text"
+            placeholder="Enter your message and press ENTER"
+            autofocus="true"
+          />
+          <button>Send</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Input;
