c13988a85a0a0597a5f2b50397c82802b2fe6092
Ana Toplicanec <ana.toplicanec@gmail.com>
2023-05-21 20:42:18 +0200

    channel id removed

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ class App extends Component {
   };
   constructor() {
     super();
-    this.drone = new window.Scaledrone('BOLaSmXEBj71ejxE', {
+    this.drone = new window.Scaledrone('YOUR_CHANNEL_ID', {
       data: this.state.member,
     });
     this.drone.on('open', (error) => {
