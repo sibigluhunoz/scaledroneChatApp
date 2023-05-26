7607769b559a925587e6fab0a84adcc66adfafdb
Ana Toplicanec <ana.toplicanec@gmail.com>
2023-05-26 12:23:53 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ class App extends Component {
   };
   constructor() {
     super();
-    this.drone = new window.Scaledrone('YOUR_CHANNEL_ID', {
+    this.drone = new window.Scaledrone('vhkmylACL3JfgtpL', {
       data: this.state.member,
     });
     this.drone.on('open', (error) => {
